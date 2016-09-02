8fdda4e9a7e31d591a7c45372c5fce24e4a404c1
Charlie Root <srikanth.birada@gmail.com>
2016-09-02 09:15:58 +0000

    changes

--- heartbeat.pl
+++
@@ -1,5 +0,0 @@
-while(1) {
-	my $time = time;
-	`echo "$time : Filebeat is alive" >>/sc/filebeat/heartbeat.log`;
-	sleep(60);
-}
--- setup_filebeat_user.sh
+++
@@ -1,1 +0,0 @@
-pw useradd filebeat -m -d /home/filebeat -c "creating filebeat user" -s /usr/local/bin/bash
